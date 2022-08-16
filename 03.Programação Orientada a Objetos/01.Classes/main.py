6526ad0e892e50929c6de5617f6020d920bb22b4
AntonioFarias <antoniotercerio@gmail.com>
2022-08-16 16:45:40 -0300

    feature(main): starting the studies of Class OPP

---
+++ 03.Programação Orientada a Objetos/01.Classes/main.py
@@ -0,0 +1,37 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz',29)
+p2 = Pessoa('João',32)
+
+
+p1.comer('maçã')
+print('--------------')
+
+
+p1.comer('maçã')
+print('--------------')
+
+p1.pararComer()
+print('--------------')
+
+p1.pararComer()
+print('--------------')
+
+p1.falar('economia')
+print('--------------')
+
+p1.pararComer()
+print('--------------')
+
+p1.falar('falar')
+print('--------------')
+
+p1.pararFalar()
+print('--------------')
+
+p2.falar('Assunto legal')
+print('--------------')
+
+p1.falar('Assunto chato')
+print('--------------')
+
