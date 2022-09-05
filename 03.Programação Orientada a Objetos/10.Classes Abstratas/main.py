cc478e8027106ddb54427fe1ac788421d9cb2a19
AntonioFarias <antoniotercerio@gmail.com>
2022-09-05 12:46:40 -0300

    feature : finishing abastract class studies

--- 03.Programação Orientada a Objetos/10.Classes Abstratas/main.py
+++ 03.Programação Orientada a Objetos/10.Classes Abstratas/main.py
@@ -1,6 +1,14 @@
 from classes.contaPoupanca import ContaPoupanca
-
+from classes.contaCorrente import ContaCorrente
 
 cp = ContaPoupanca(1111,2222,0)
 cp.depositar(10)
 cp.detalhes()
+cp.sacar(10)
+
+print('################')
+
+cc = ContaCorrente(1111,3333,0,500)
+cc.detalhes()
+cc.depositar(100)
+cc.sacar(50)
