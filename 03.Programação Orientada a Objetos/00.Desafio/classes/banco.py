11677c8bdd978991ab6269b3988b0676b5aaa2f8
AntonioFarias <antoniotercerio@gmail.com>
2022-09-26 20:12:53 -0300

    fix : indentation-error

--- 03.Programação Orientada a Objetos/00.Desafio/classes/banco.py
+++ 03.Programação Orientada a Objetos/00.Desafio/classes/banco.py
@@ -4,21 +4,21 @@ class Banco:
         self.clientes = []
         self.contas = []
 
-        def inserir_clientes(self,cliente):
-            self.clientes.append(cliente)
+    def inserir_clientes(self,cliente):
+        self.clientes.append(cliente)
+    
+    def inserir_conta(self, conta):
+        self.contas.append(conta)
+
+    def autenticar(self, cliente):
+        if cliente not in self.clientes:
+            return False
         
-        def inserir_conta(self, conta):
-            self.contas.append(conta)
+        if cliente.conta not in self.contas:
+            return False
 
-        def autenticar(self, cliente):
-            if cliente not in self.clientes:
-                return False
-            
-            if cliente.conta not in self.contas:
-                return False
-
-            if cliente.conta.agencia not in self.agencias:
-                return False
-            
-            return True 
+        if cliente.conta.agencia not in self.agencias:
+            return False
+        
+        return True 
         
