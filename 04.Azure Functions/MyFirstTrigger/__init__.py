4573cb868e02b32f7e947d3a89a5a645c776d319
AntonioFarias <antoniotercerio@gmail.com>
2022-10-05 21:05:05 -0300

    feat: function-return-public-ip

--- 04.Azure Functions/MyFirstTrigger/__init__.py
+++ 04.Azure Functions/MyFirstTrigger/__init__.py
@@ -2,9 +2,18 @@ import logging
 
 import azure.functions as func
 
+from requests import get
+
+
+def GetIPMan():
+    myip = get('https://ip4.seeip.org/').text
+    print(f"Look! I just found my public IP {myip}")
+    return f"I just found my public IP here {myip}"
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    GetIPMan()
 
     name = req.params.get('name')
     if not name:
