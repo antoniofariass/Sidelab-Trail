b91f7db9a82d6f4fe051762ecf3197c8044d31f1
AntonioFarias <antoniotercerio@gmail.com>
2022-10-17 16:53:54 -0300

    config: yaml-file

---
+++ 05.Lint de Codigo/src/start.py
@@ -0,0 +1,20 @@
+# docstring
+def soma(num_1: int, num_2: int) -> int:
+    """Somando dois números
+    :param - num_1: primeiro numero da soma
+             num_2: segundo numero da soma
+    :return soma_dois_numeros
+    """
+
+    soma_dois_numeros = num_1 + num_2
+    return soma_dois_numeros
+
+
+def minha_funcao(
+    argumento_muitogrande,
+    argumento_muitogrande2,
+    argumento_muitogrande3,
+    argumento_muitogrande4,
+):
+    print(argumento_muitogrande)
+    return True
